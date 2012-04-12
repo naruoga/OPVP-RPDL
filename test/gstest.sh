a38d8ba2f7bb00fafb2be759bbbf4d91072bc0d9
Naruhiko Ogasawara <naruoga@gmail.com>
2012-04-12 19:45:58 +0900

    テスト用スクリプトのリンクライブラリが古いままだったので修正

--- test/gstest.sh
+++ test/gstest.sh
@@ -2,6 +2,6 @@
 
 date
 
-gs -r600 -sDEVICE=opvp -sDriver=libopvpnull.so -sModel=model_name -sJobInfo="job_info" -q -dBATCH -dSAFER -dQUIET -dNOPAUSE -sOutputFile=- $1
+gs -r600 -sDEVICE=opvp -sDriver=libopvprpdl.so -sModel=model_name -sJobInfo="job_info" -q -dBATCH -dSAFER -dQUIET -dNOPAUSE -sOutputFile=- $1
 
 date
